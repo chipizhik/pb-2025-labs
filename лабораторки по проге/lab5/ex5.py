106e36e7225a5a0b3a8ab394ff10e780563d980c
chipizhik <david.yes.rf@gmail.com>
2025-11-13 16:19:27 +1000

    labs

--- лабораторки по проге/lab5/ex5.py
+++
@@ -1,9 +0,0 @@
-def palindrome(text):
-    text = text.lower()
-    bez_probelov = text.replace(' ', '')
-    return bez_probelov == bez_probelov[::-1]
-input_text = input()
-if palindrome(input_text):
-    print("Да")
-else:
-    print("Нет")
--- лабораторки по проге/lab5a/ex1.py
+++
@@ -1,3 +0,0 @@
-tasks = [("Проверить почту", 31), ("Написать отчёт", 111), ("Позвонить клиенту", 2), ("позвонить клиенту", 4)]
-sorted_tasks = sorted(tasks, key=lambda task: task[1])
-print(sorted_tasks)
